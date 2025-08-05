cf05f269f70e52cff012d115edfc60c8a1a2e947
manen <49951816+manen@users.noreply.github.com>
2025-08-05 21:22:28 +0200

    move `TextureID` into own file

---
+++ game/src/textures/texture_id.rs
@@ -0,0 +1,58 @@
+use std::borrow::Cow;
+
+use strum::{EnumIter, IntoEnumIterator};
+
+/// an enum for every texture we can use \
+/// extensible in the future by adding an Other(u64) and have some sort of setup that hashes their
+/// resource_path, just so it's faster \
+/// this id is used a lot in every tick
+#[derive(Clone, Debug, PartialEq, Eq, Hash, EnumIter)]
+pub enum TextureID {
+	Transparent,
+
+	SmallExtractor,
+	ChannelConsumer,
+
+	Stone,
+	IronOre,
+	CoalOre,
+
+	Coal,
+	RawIron,
+
+	ConveyorTop,
+}
+impl TextureID {
+	/// none just becomes transparent
+	pub const fn resource_path(&self) -> Cow<'static, str> {
+		match self {
+			TextureID::Transparent => Cow::Borrowed("textures/transparent.png"),
+
+			TextureID::SmallExtractor => Cow::Borrowed(
+				"https://static.wikia.nocookie.net/minecraft_gamepedia/images/e/e7/Diamond_Pickaxe_JE3_BE3.png/revision/latest/scale-to-width/360?cb=20250628224016",
+			),
+			TextureID::ChannelConsumer => Cow::Borrowed("textures/channel-consumer.png"),
+
+			TextureID::Stone => Cow::Borrowed("textures/stone.png"),
+			TextureID::IronOre => Cow::Borrowed("textures/iron_ore.png"),
+			TextureID::CoalOre => Cow::Borrowed("textures/coal_ore.png"),
+			TextureID::Coal => Cow::Borrowed(
+				"https://static.wikia.nocookie.net/minecraft/images/a/a7/Coal.png/revision/latest/scale-to-width/360?cb=20200814153155",
+			),
+			TextureID::RawIron => Cow::Borrowed(
+				"https://static.wikia.nocookie.net/minecraft_gamepedia/images/d/d2/Raw_Iron_JE3_BE2.png/revision/latest?cb=20210421181435",
+			),
+			TextureID::ConveyorTop => Cow::Borrowed("textures/conveyor-top.png"),
+		}
+	}
+}
+
+pub fn all_textures() -> impl Iterator<Item = TextureID> {
+	// &[
+	// 	TextureID::Stone,
+	// 	TextureID::IronOre,
+	// 	TextureID::CoalOre,
+	// 	TextureID::RawIron,
+	// ]
+	TextureID::iter()
+}
--- todo.md
+++ todo.md
@@ -18,12 +18,22 @@ the 'story' of the tutorial:
 
 and this is about as long as we can get right now cause we're missing some crucial features:
 
-- [ ] disposal/collection/counting of resources by wiring them out of the world (or maybe into a house where the player lives? that'd add a nice touch) <!-- this next!  -->
+- [x] disposal/collection/counting of resources by wiring them out of the world (or maybe into a house where the player lives? that'd add a nice touch) <!-- this next!  -->
 
 - a block like that is super easy to make, `EBuilding` is already only `Clone`, just give it a Sender and let it send all the resources it receives
 
 - the side of the map would be much more low-level, just checking to see if resources are trying to pass into a building outside bounds and add that to the count
 
 - **verdict: building is easier and could be made more engaging too**
+
+---
+
+- [ ] new toolbar with textures and name only on hover (fits better and looks better)
+
+- for that, we need to do something about textures since dropping textures actually drops the texture so that can't be cloned <!-- (*just put `sui::Texture`'s texture into an Arc<_>*) -->
+
+- toolbar has no way of receiving a `&Textures` at render-time, so when creating the toolbar we could clone Textures and make it only hold an `Arc<HashMap<TextureID, Texture>>`, so it's cheap and easy to clone
+
+---
 
 - [ ] i'll probably need to put all the maps into an ugly ass broke ass shitty ass Arc<Mutex<_>> but ion think there's another option for much longer (off-thread ticking, checking to see if two buildings are connected with conveyors from the tutorial, etc)
