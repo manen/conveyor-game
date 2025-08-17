a797babd83bd3fe75db47c541f0da9195fdaa3f5
manen <49951816+manen@users.noreply.github.com>
2025-08-18 00:29:20 +0200

    move `err_page_customizable` into `comp_extra`

--- crates/comp_extra/src/lib.rs
+++ crates/comp_extra/src/lib.rs
@@ -2,3 +2,5 @@ mod two;
 pub use two::*;
 mod tooltip_on_hover;
 pub use tooltip_on_hover::*;
+mod err_page;
+pub use err_page::*;
--- crates/game/src/comp/err_page.rs
+++ crates/game/src/comp/err_page.rs
@@ -1,33 +1,9 @@
 use std::fmt::{Debug, Display};
 
 use stage_manager::StageChange;
-use sui::{DynamicLayable, Layable, LayableExt};
+use sui::{DynamicLayable, Layable};
 
 use crate::scripts::main::main_menu;
-
-pub fn err_page_customizable<E: Debug + Display>(
-	err: E,
-	mut return_to_menu: Option<StageChange<'static>>,
-) -> impl Layable + Debug + 'static {
-	let display = format!("{err}");
-	let debug = format!("{err:?}");
-
-	let err_info = sui::div([
-		sui::custom(sui::text(display, 32).centered()),
-		sui::custom(sui::text(debug, 24)),
-	]);
-
-	let return_to_menu = match return_to_menu {
-		Some(_) => vec![sui::text("return to main menu", 24).clickable(move |_| {
-			return_to_menu
-				.take()
-				.expect("the main menu stagechange was taken already")
-		})],
-		None => vec![],
-	};
-	let return_to_menu = sui::div(return_to_menu).to_bottom();
-	err_info.overlay(return_to_menu)
-}
 
 pub fn err_page<E: Debug + Display>(err: E) -> impl Layable + Debug + 'static {
 	let main_menu = main_menu();
@@ -35,7 +11,7 @@ pub fn err_page<E: Debug + Display>(err: E) -> impl Layable + Debug + 'static {
 		StageChange::simple_only_debug(a)
 	});
 
-	err_page_customizable(err, Some(main_menu))
+	comp_extra::err_page_customizable(err, Some(main_menu))
 }
 
 pub fn handle_result<E: Debug + Display, T: Layable + Debug + 'static>(
