b0f598a3a167cb9718332ba2adfc7791f750f623
manen <49951816+manen@users.noreply.github.com>
2025-08-20 14:21:49 +0200

    change tick rate to 13 times a second

--- crates/game_core/src/prov/mod.rs
+++ crates/game_core/src/prov/mod.rs
@@ -1,7 +1,10 @@
 // GameData and GameProvider
 
 use std::time::Duration;
-pub const GAME_TICK_FREQUENCY: Duration = Duration::from_millis(1000 / 20);
+pub const GAME_TICK_FREQUENCY: Duration = const {
+	let millis = Duration::from_secs(1).as_millis() / 13;
+	Duration::from_millis(millis as u64)
+};
 
 mod data;
 pub use data::*;
